1a9874a2080938fb1a149aa89042804e66707c64
sieveLau <sievelau@gmail.com>
2021-06-06 15:21:20 +0800

    reformat

--- md5.cpp
+++ md5.cpp
@@ -1,83 +1,72 @@
-#include "MD5.hpp"
+#include "md5.hpp"
 
 namespace sieve {
 namespace hash {
-std::array<unsigned char, 16> hash::md5(const unsigned char *bytes,
-                                        const size_t bytes_to_cal)
-{
-    std::array<unsigned char, MD5_DIGEST_LENGTH> result;
-    EVP_Digest(bytes, bytes_to_cal, result.data(),NULL, EVP_md5(),NULL);
-    // MD5(bytes, bytes_to_cal, result.data());
-    return result;
+std::array<unsigned char, 16> md5(const unsigned char *bytes,
+                                  const size_t bytes_to_cal) {
+  std::array<unsigned char, MD5_DIGEST_LENGTH> result;
+  EVP_Digest(bytes, bytes_to_cal, result.data(), NULL, EVP_md5(), NULL);
+  // MD5(bytes, bytes_to_cal, result.data());
+  return result;
 }
 
-std::array<unsigned char, 16> hash::md5(const std::string &str)
-{
-    return md5(reinterpret_cast<const unsigned char *>(str.c_str()), str.size());
+std::array<unsigned char, 16> md5(const std::string &str) {
+  return md5(reinterpret_cast<const unsigned char *>(str.c_str()), str.size());
 }
 
-void MD5::dirty_reset() noexcept
-{
-    buffer_ = nullptr;
-    finish_ = true;
+void MD5::dirty_reset() noexcept {
+  buffer_ = nullptr;
+  finish_ = true;
 }
 
-MD5::MD5()
-    : buffer_(EVP_MD_CTX_new()), finish_(false)
-{
-    EVP_DigestInit_ex(buffer_, EVP_md5(), nullptr);
+MD5::MD5() : buffer_(EVP_MD_CTX_new()), finish_(false) {
+  EVP_DigestInit_ex(buffer_, EVP_md5(), nullptr);
 }
 
-MD5::MD5(MD5 &&another) noexcept
-    : MD5()
-{
-    swap(*this,another);
+MD5::MD5(MD5 &&another) noexcept : MD5() { swap(*this, another); }
+
+MD5 &MD5::operator=(MD5 another) {
+  swap(*this, another);
+  return *this;
 }
 
-MD5 & MD5::operator=(MD5 another)
-{
-    swap(*this,another);
-    return *this;
+void MD5::update(const std::string &data) {
+  EVP_DigestUpdate(buffer_, data.c_str(), data.size());
 }
 
-void MD5::update(const std::string &data)
-{
-    EVP_DigestUpdate(buffer_, data.c_str(), data.size());
+// 读取bytes中的bytes_to_cal个byte来update digeset
+void MD5::update(const unsigned char *bytes, const size_t bytes_to_cal) {
+  EVP_DigestUpdate(buffer_, bytes, bytes_to_cal);
 }
 
-void MD5::update(const unsigned char * bytes, const size_t bytes_to_cal)
-{
-    EVP_DigestUpdate(buffer_,bytes,bytes_to_cal);
+// 返回截止到目前的digest
+// 调用之后还可以继续update
+std::array<unsigned char, MD5_DIGEST_LENGTH> MD5::digest() {
+  std::array<unsigned char, digest_size> result;
+  EVP_MD_CTX *temp_copy = EVP_MD_CTX_new();
+  EVP_MD_CTX_copy(temp_copy, buffer_);
+  EVP_DigestFinal(temp_copy, result.data(), nullptr);
+  return result;
 }
 
-
-std::array<unsigned char, MD5_DIGEST_LENGTH> MD5::digest()
-{
-    std::array<unsigned char, digest_size> result;
-    EVP_MD_CTX *temp_copy=EVP_MD_CTX_new();
-    EVP_MD_CTX_copy(temp_copy, buffer_);
-    EVP_DigestFinal(temp_copy, result.data(), nullptr);
-    return result;
+/**
+ * @brief 返回截止到目前的digest的hex
+ *
+ * @return std::string digest的hex
+ */
+std::string MD5::hexdigest() {
+  auto temp = digest();
+  return util::to_hex(temp);
 }
 
-std::string MD5::hexdigest()
-{
-    auto temp = digest();
-    return util::to_hex(temp);
+MD5::~MD5() {
+  EVP_MD_CTX_free(buffer_);
+  finish_ = true;
 }
 
-MD5::~MD5()
-{
-    EVP_MD_CTX_free(buffer_);
-    finish_ = true;
+MD5::MD5(const MD5 &another) : buffer_(nullptr), finish_(another.finish_) {
+  buffer_ = EVP_MD_CTX_new();
+  EVP_MD_CTX_copy_ex(buffer_, another.buffer_);
 }
-
-MD5::MD5(const MD5 &another):buffer_(nullptr),finish_(another.finish_)
-{
-    buffer_=EVP_MD_CTX_new();
-    EVP_MD_CTX_copy_ex(buffer_,another.buffer_);
-}
-
-
-}
-}
+} // namespace hash
+} // namespace sieve
--- md5.hpp
+++ md5.hpp
@@ -1,69 +1,66 @@
 #pragma once
+#include "external/util/util.hpp"
 #include <array>
 #include <openssl/evp.h>
-#include "external/util/util.hpp"
 
 namespace sieve {
 namespace hash {
 const int MD5_DIGEST_LENGTH = 16;
-/**
- * \brief calculate the md5 of the parameter.
- * \param bytes content to calculate md5
- * \param include_string_terminator If true, treat '\0' as part of the input. Default false.
- * \return a unsigned char std::array containing the digest
- */
-template<int N>
-auto md5(const unsigned char (&bytes)[N], bool include_string_terminator = false)
-{
-    std::array<unsigned char, MD5_DIGEST_LENGTH> result;
-    unsigned long bytes_to_cal = N;
-    if (!include_string_terminator && bytes[N - 1] == (unsigned char) '\0') {
-        --bytes_to_cal;
-    }
-    EVP_Digest(bytes, bytes_to_cal, result.data(),NULL, EVP_md5(),NULL);
-    return result;
+
+/// calculate the md5sum of a byte array
+/// array size auto deduce
+template <int N>
+auto md5(const unsigned char (&bytes)[N],
+         bool include_string_terminator = false) {
+  std::array<unsigned char, MD5_DIGEST_LENGTH> result;
+  unsigned long bytes_to_cal = N;
+  if (!include_string_terminator && bytes[N - 1] == (unsigned char)'\0') {
+    --bytes_to_cal;
+  }
+  EVP_Digest(bytes, bytes_to_cal, result.data(), NULL, EVP_md5(), NULL);
+  return result;
 }
 
+// calculate the md5sum of byte array.
+// 2nd param indicates the number of bytes to read
+std::array<unsigned char, MD5_DIGEST_LENGTH> md5(const unsigned char *bytes,
+                                                 size_t bytes_to_cal);
 
-std::array<unsigned char, MD5_DIGEST_LENGTH> md5(const unsigned char *bytes, size_t bytes_to_cal);
-
+// calculate the md5sum of std::string.
 std::array<unsigned char, MD5_DIGEST_LENGTH> md5(const std::string &str);
 
-class MD5
-{
-    EVP_MD_CTX *buffer_;
-    bool finish_;
-    void dirty_reset() noexcept;
+class MD5 {
+  EVP_MD_CTX *buffer_;
+  bool finish_;
+  void dirty_reset() noexcept;
+
 public:
-    const static size_t digest_size = MD5_DIGEST_LENGTH;
+  const static size_t digest_size = MD5_DIGEST_LENGTH;
 
-    explicit MD5();
+  explicit MD5();
 
-    friend void swap(MD5 &left, MD5 &right)
-    {
-        using std::swap;
-        swap(left.buffer_, right.buffer_);
-        swap(left.finish_, right.finish_);
-    }
+  friend void swap(MD5 &left, MD5 &right) {
+    using std::swap;
+    swap(left.buffer_, right.buffer_);
+    swap(left.finish_, right.finish_);
+  }
 
-    MD5(const MD5 &);
-    MD5(MD5 &&) noexcept;
-    MD5 &operator=(MD5);
+  MD5(const MD5 &);
+  MD5(MD5 &&) noexcept;
+  MD5 &operator=(MD5);
 
-    void update(const std::string &);
-    void update(const unsigned char *, size_t);
+  void update(const std::string &);
+  void update(const unsigned char *, size_t);
 
-    template<size_t SIZE>
-    void update(std::array<unsigned char, SIZE> bytes)
-    {
-        EVP_DigestUpdate(buffer_, bytes.data(), SIZE);
-    }
+  template <size_t SIZE> void update(std::array<unsigned char, SIZE> bytes) {
+    EVP_DigestUpdate(buffer_, bytes.data(), SIZE);
+  }
 
-    std::array<unsigned char, MD5_DIGEST_LENGTH> digest();
-    std::string hexdigest();
+  std::array<unsigned char, MD5_DIGEST_LENGTH> digest();
+  std::string hexdigest();
 
-    ~MD5();
+  ~MD5();
 };
 
-}
-}
+} // namespace hash
+} // namespace sieve
